15c8d2c3e0d69c5fdccdcde6bc1de4db23b8b069
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2025-09-01 00:10:07 +0000

    first chapter: complete

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_options|
-    driver_options.add_argument("headless")
-    driver_options.add_argument("no-sandbox")
-    driver_options.add_argument("disable-gpu")
-    driver_options.add_argument("disable-dev-shm-usage")
-  end
+  driven_by :selenium, using: :headless_firefox, screen_size: [ 1400, 1400 ]
 end
